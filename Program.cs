50abb5326b398729de9adc51765254f07cf03102
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-06-26 08:48:23 -0600

    Added case condition to replace esleif statement

--- Program.cs
+++ Program.cs
@@ -7,29 +7,57 @@ namespace c_sharp_intro
         public static object Cosnsole { get; private set; }
 
         static void Main(string[] args)
-        {
-            int  myNum = 3;
-            while (myNum > 0)// Iterates/loops while the condition is TRUE.
-            {
-                Console.WriteLine("Current number is: {0}", myNum);
-                myNum--;
-            }
-            /*for loop has 3 semi-colon-separated componenets:
-               1)Assignement 
-               2)[Termination condition]
-               3)Iteration or looping
-            */
+        {  
+            //Guide the user
+            Console.WriteLine("Please enter add/subtract/multiply/divide"); 
 
-            for (int i = 0; i < 5; i+=2)
+            //collect user input
+            string userInput;
+            while ((userInput = Console.ReadLine())!= "exit")//Loops.....
             {
-                Console.WriteLine("Current (for) number is: {0}", i);
-            }
-            //If we need to iterate through a list, use foreach!
-            //@link https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/foreach-in
-            string[] myList = {"cat", "turtle", "lizard", "frog", "dog"};
-            foreach (string listItem in myList)
-            {
-                Console.WriteLine("Current item is {0}", listItem);
+                int firstNum;
+                int secondNum;
+                int result;
+                //Console.WriteLine("Looping.......");
+
+                //switch statements are similar to if/else 
+                switch(userInput)   //Here we enter the value we want to enter.
+                {//cases are us testing the above against specific values
+                    case "add":
+                        Console.WriteLine("Enter first number for addition:");
+                        firstNum = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter first number for addition:");
+                        secondNum = Int32.Parse(Console.ReadLine());
+                        result = firstNum + secondNum;
+                        Console.WriteLine("{0} + {1} = {2}", firstNum, secondNum, result);
+                        break;
+                    case "subtract":
+                        Console.WriteLine("Enter first number for subtraction:");
+                        firstNum = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter first number for subtraction:");
+                        secondNum = Int32.Parse(Console.ReadLine());
+                        result = firstNum - secondNum;
+                        Console.WriteLine("{0} - {1} = {2}", firstNum, secondNum, result);
+                        break;
+                    case "multiply":
+                        firstNum = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter first number for multiplication:");
+                        secondNum = Int32.Parse(Console.ReadLine());
+                        result = firstNum * secondNum;
+                        Console.WriteLine("{0} * {1} = {2}", firstNum, secondNum, result);
+                        break;
+                    case "divide":
+                        Console.WriteLine("enter the first number to divide");
+                        firstNum = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the second number to divide");
+                        secondNum = Int32.Parse(Console.ReadLine());
+                        result = firstNum / secondNum;
+                        Console.WriteLine("{0} / {1} = {2}", firstNum, secondNum, result);
+                        break;
+                    default:
+                        Console.WriteLine("Unrecognized operator entered");
+                        break;
+                }//end switch statement
             }
         }
     }
